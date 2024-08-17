4e0cc3eae44a2c6728c499d1f27aed9499339f20
James Yang <jamesyang916@gmail.com>
2024-08-17 16:15:27 -0700

    Add super polished version

--- choosi/distr.py
+++ choosi/distr.py
@@ -1,9 +1,15 @@
 from .choosi_core.distr import (
     compute_cdf,
     compute_cdf_root,
+    compute_weights,
+)
+from scipy.special import (
+    roots_hermite,
+    roots_laguerre,
 )
 from scipy.optimize import root_scalar
 from scipy.stats import norm, rv_discrete
+from typing import Union
 import numpy as np
 
 class WeightedNormal(object):
@@ -117,9 +123,10 @@ class WeightedNormal(object):
 
     
 class WeightedNormal2(object):
-    """
+    """TODO
 
     The CDF is given by 
+
     ..math::
         \\begin{align*}
             F(z; \mu) := \\frac{
@@ -129,85 +136,174 @@ class WeightedNormal2(object):
             }
         \\end{align*}
 
+    The weight function :math:`w` is given by
+
+    ..math::
+        \\begin{align*}
+            w(x) = \Phi((u-x)/s) - \Phi((\\ell-x)/s)
+        \\end{align*}
+
+    where :math:`\Phi` is the standard normal CDF.
+
     Parameters
     ----------
     sigma : float
         The scale parameter :math:`\\sigma`.
-    hermite_roots : ndarray
-        The Hermite roots as given by :func:`scipy.special.roots_hermite`
-        multiplied by ``np.sqrt(2) * sigma``.
-    hermite_weights : ndarray
-        The Hermite weights as given by :func:`scipy.special.roots_hermite`.
-    weights : ndarray, optional
-        Any non-negative weights :math:`w(x)` evaluated at ``hermite_roots``.
-        If ``None``, it is assumed that :math:`w(x) \\equiv 1``.
+    w_s : float
+        The scale argument :math:`s` to :math:`w(\cdot)`.
+    w_l : float
+        The lower argument :math:`\\ell` to :math:`w(\cdot)`.
+    w_u : float
+        The upper argument :math:`u` to :math:`w(\cdot)`.
+    laguerre : ndarray, optional
+        The Laguerre roots and weights as given by :func:`scipy.special.roots_laguerre`.
+        If ``None``, it is initialized with ``scipy.special.roots_laguerre(100)``.
+        Default is ``None``.
+    hermite : ndarray, optional
+        The Hermite roots and weights as given by :func:`scipy.special.roots_hermite`.
+        If ``None``, it is initialized with ``scipy.special.roots_hermite(10)``.
         Default is ``None``.
     """
     def __init__(
         self,
         sigma: float,
-        hermite_roots: np.ndarray,
-        hermite_weights: np.ndarray,
-        weights: np.ndarray =None,
+        w_s: float,
+        w_l: float,
+        w_u: float,
+        laguerre: Union[tuple, None] =None,
+        hermite: Union[tuple, None] =None,
     ):
         self.sigma = sigma
-        self.var = sigma ** 2
-        self.hermite_roots = hermite_roots
-        self.weights = hermite_weights
-        if not (weights is None):
-            self.weights = self.weights * weights
+        if self.sigma <= 0:
+            raise ValueError("sigma must be > 0.")
+        self.scale = sigma * np.sqrt(2)
+        self.w_args = (w_s / self.scale, w_l / self.scale, w_u / self.scale)
+        if self.w_args[0] <= 0:
+            raise ValueError("w_s must be > 0.")
+        if self.w_args[1] > self.w_args[2]:
+            raise ValueError("w_l must be <= w_u.")
+        self.laguerre = laguerre
+        self.hermite = hermite
+        if laguerre is None:
+            self.laguerre = roots_laguerre(100)
+        if hermite is None:
+            self.hermite = roots_hermite(10)
+
+    def _compute_weights(self, z):
+        return compute_weights(z, *self.w_args)
+
+    def _compute_w_pool_L(self, a_z):
+        return self.laguerre[1] * self._compute_weights(a_z - self.laguerre[0])
+
+    def _compute_cdf(self, mu_z, a_z, w_pool_L):
+        return compute_cdf(
+            mu_z, 
+            a_z,
+            self.laguerre[0],
+            w_pool_L,
+            self.hermite[0], 
+            self.hermite[1],
+            *self.w_args,
+        )
+
+    def _compute_cdf_root(self, level, lower, upper, a_z, w_pool_L, tol):
+        return compute_cdf_root(
+            level, 
+            lower, 
+            upper, 
+            a_z, 
+            self.laguerre[0],
+            w_pool_L,
+            self.hermite[0], 
+            self.hermite[1],
+            *self.w_args,
+            tol,
+        )
 
     def cdf(
         self,
         obs_val,
         mu,
     ):
-        # compute the number of roots <= obs_val
-        # TODO: binary search can make it slightly faster.
-        s = np.sum(self.hermite_roots <= obs_val)
-        return compute_cdf(mu / self.var, s, self.hermite_roots, self.weights)
+        mu_z = mu / self.scale
+        a_z = obs_val / self.scale
+        w_pool_L = self._compute_w_pool_L(a_z)
+        return self._compute_cdf(mu_z, a_z, w_pool_L)
         
     def find_ci(
         self,
         obs_val: float,
         level: float,
+        verbose: bool =False,
+        tol: float =1e-9,
     ):
-        # compute the number of roots <= obs_val
-        # TODO: binary search can make it slightly faster.
-        s = np.sum(self.hermite_roots <= obs_val)
+        # standardize observed value
+        a_z = obs_val / self.scale
 
-        # construct a grid of nu = mu / sigma ** 2
-        # TODO: if obs_val / sigma is very large in magnitude,
-        # the gridding of nu seems to be very bad!
-        # Needs a more stable initial choice of nus.
-        center = obs_val / self.var
-        width = 3 / self.sigma
-        n_gridpts = 1000
-        nus_orig = np.linspace(center - width, center + width, n_gridpts)
+        # optimization: compute pooled weights
+        w_pool_L = self._compute_w_pool_L(a_z)
 
-        # compute lower bound
+        # collect cdf arguments
+        cdf_args = (
+            a_z,
+            w_pool_L,
+        )
+
+        # construct an initial grid of nu = mu / self.scale
+        center = a_z
+        width = 2
+
+        # compute the range of nu containing lower_level
+        def _compute_range(level, direction):
+            nu_lower = center - width
+            nu_upper = center + width
+            iters = 0
+
+            # set the initial moving and following values
+            nu_moving, nu_following = (
+                (nu_upper, -np.inf)
+                if direction == 1 else
+                (nu_lower, np.inf)
+            )
+
+            # keep sliding the window until it contains the desired level
+            cdf = self._compute_cdf(nu_moving, *cdf_args)
+            while direction * (cdf - level) > 0:
+                nu_dist = abs(nu_moving - nu_following) if iters else width
+                nu_following = nu_moving
+                nu_moving += direction * 2 * nu_dist
+                cdf = self._compute_cdf(nu_moving, *cdf_args)
+                iters += 1
+
+            # order the window in increasing order
+            nu_lower, nu_upper = (
+                (nu_following, nu_moving)
+                if direction == 1 else
+                (nu_moving, nu_following)
+            )
+
+            return nu_lower, nu_upper, iters
+            
+        # compute desired symmetric levels
         lower_level = (1-level) / 2
-        nus = np.copy(nus_orig)
-        Ui = compute_cdf_root(lower_level, nus, s, self.hermite_roots, self.weights)
-        while (Ui <= 0 or Ui >= nus.size):
-            if Ui <= 0:
-                nus -= width
-            else:
-                nus += width
-            Ui = compute_cdf_root(lower_level, nus, s, self.hermite_roots, self.weights)
-        U = nus[Ui]
-
-        # compute upper bound
         upper_level = (1+level) / 2
-        nus = np.copy(nus_orig)
-        Li = compute_cdf_root(upper_level, nus, s, self.hermite_roots, self.weights)
-        while (Li <= 0 or Li >= nus.size):
-            if Li <= 0:
-                nus -= width
-            else:
-                nus += width
-            Li = compute_cdf_root(upper_level, nus, s, self.hermite_roots, self.weights)
-        L = nus[Li]
 
-        # return the CI on the correct scale
-        return self.var * L, self.var * U
+        # find range containing lower_level
+        U_lower, U_upper, U_search_iters = _compute_range(lower_level,  1)
+        L_lower, L_upper, L_search_iters = _compute_range(upper_level, -1)
+        L_upper, U_lower = (
+            min(L_upper, U_upper),
+            max(L_lower, U_lower),
+        )
+
+        # compute CI bounds
+        U, U_iters = self._compute_cdf_root(lower_level, U_lower, U_upper, *cdf_args, tol)
+        L, L_iters = self._compute_cdf_root(upper_level, L_lower, L_upper, *cdf_args, tol)
+
+        # return CI on original scale
+        L, U = self.scale * L, self.scale * U
+
+        if verbose:
+            return L, U, L_search_iters, U_search_iters, L_iters, U_iters,
+
+        return L, U
--- choosi/src/distr.cpp
+++ choosi/src/distr.cpp
@@ -1,36 +1,62 @@
 #include "decl.hpp"
 #include <adelie_core/util/types.hpp>
+#include <unsupported/Eigen/SpecialFunctions>
 
 namespace ad = adelie_core;
 namespace py = pybind11;
 
+template <class ZType, class ValueType>
+auto compute_weights(
+    const ZType& z,
+    ValueType s_z,
+    ValueType l_z,
+    ValueType u_z
+)
+{   
+    const auto s_z_scaled = s_z / M_SQRT1_2;
+    return 0.5 * (
+        ((z-u_z) / s_z_scaled).erfc()
+        - 
+        ((z-l_z) / s_z_scaled).erfc()
+    );
+}
+
 /**
- * Computes the CDF function given by
+ * Computes the CDF given by
  * 
- *      \frac{
- *          \sum_{i \leq s} w(x_i) \exp(x_i \mu) 
+ *      \exp(1/4 + a_z - mu_z) \frac{
+ *          \sum_i \xi_i^L 
+ *          w(a_z-z_i^L) 
+ *          \exp(-(z_i^L-(a_z-mu_z+1/2))^2)
  *      }{
- *          \sum_{i} w(x_i) \exp(x_i \mu) 
+ *          \sum_i \xi_i^H
+ *          w(mu_z+z_i^H) 
  *      }
- * 
- * @param mu    mu value.
- * @param s     s value.
- * @param x     a grid of values in increasing order.
- * @param w     non-negative weights corresponding to x.
+ *      
  */
 template <class ValueType>
 ValueType compute_cdf(
-    ValueType mu,
-    size_t s,
-    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& x,
-    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& w
+    ValueType mu_z,
+    ValueType a_z,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& z_L,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& w_pool_L,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& z_H,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& xi_H,
+    ValueType s_z,
+    ValueType l_z,
+    ValueType u_z
 )
 {
-    const auto n = x.size();
-    const auto shift = (mu > 0) ? x[n-1] : x[0];
-    const auto expr = w * (mu * (x - shift)).exp();
-    const auto numer = expr.head(s).sum();
-    const auto denom = numer + expr.tail(n-s).sum();
+    using value_t = ValueType;
+    const auto diff = a_z - mu_z;
+    const auto numer = (
+        w_pool_L
+        * (-(z_L - (diff + 0.5)).square() + 0.25 + diff).exp()
+    ).sum();
+    const auto denom = (
+        xi_H 
+        * compute_weights(mu_z + z_H, s_z, l_z, u_z)
+    ).sum();
     return numer / (denom + (denom <= 0));
 }
 
@@ -39,45 +65,73 @@ ValueType compute_cdf(
  * is less than or equal to the level at that mu.
  * 
  * @param level   desired level of CDF.
- * @param mu    a grid of mu values in increasing order.
+ * @param lower lower bound on mu.
+ * @param upper upper bound on mu.
  * @param s     s value.
  * @param x     a grid of values in increasing order.
  * @param w     non-negative weights corresponding to x.
  */
 template <class ValueType>
-size_t compute_cdf_root(
+auto compute_cdf_root(
     ValueType level,
-    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& mu,
-    size_t s,
-    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& x,
-    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& w
+    ValueType lower,
+    ValueType upper,
+    ValueType a_z,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& z_L,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& w_pool_L,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& z_H,
+    const Eigen::Ref<const ad::util::rowvec_type<ValueType>>& xi_H,
+    ValueType s_z,
+    ValueType l_z,
+    ValueType u_z,
+    ValueType tol
 )
 {
+    using value_t = ValueType;
+
     const auto alpha = 1 - level;
-    const auto sf = [&](auto m) { 
-        return 1-compute_cdf<ValueType>(m, s, x, w); 
+    const auto compute_sf = [&](auto mu_z) { 
+        return 1-compute_cdf<value_t>(
+            mu_z, a_z, z_L, w_pool_L, z_H, xi_H, s_z, l_z, u_z
+        ); 
     };
 
-    // binary search to find the first mu[i] such that sf(mu[i]) >= alpha.
-    size_t begin = 0;
-    size_t count = mu.size();
-    while (count > 0)
+    size_t iters = 0;
+
+    // compute SF at the two bounds and early exit if not sf(lower) < alpha < sf(upper)
+    value_t sf_lower = compute_sf(lower);
+    if (sf_lower >= alpha) return std::make_tuple(lower, iters);
+    value_t sf_upper = compute_sf(upper);
+    if (sf_upper <= alpha) return std::make_tuple(upper, iters);
+
+    const auto compute_cand = [&]() {
+        return 0.5 * (lower + upper);
+    };
+
+    while (std::abs(sf_upper - sf_lower) > tol)
     {
-        const auto step = count >> 1;
-        const auto cand = begin + step;
-        if (sf(mu[cand]) < alpha) {
-            begin = cand + 1;
-            count -= step + 1;
-        } else {
-            count = step;
-        }
+        ++iters;
+        const auto mu_cand = compute_cand();
+        const auto sf_cand = compute_sf(mu_cand);
+        if (std::abs(sf_cand - alpha) <= tol) break;
+        lower = (sf_cand < alpha) ? mu_cand : lower;
+        upper = (sf_cand < alpha) ? upper : mu_cand;
     }
 
-    return begin;
+    return std::make_tuple(compute_cand(), iters);
 }
 
 void register_distr(py::module_& m)
 {
+    m.def("compute_weights", [](
+        const Eigen::Ref<const ad::util::rowvec_type<double>>& z,
+        double s_z,
+        double l_z,
+        double u_z
+    ) {
+        ad::util::rowvec_type<double> out = compute_weights(z, s_z, l_z, u_z);
+        return out;
+    });
     m.def("compute_cdf", compute_cdf<double>);
     m.def("compute_cdf_root", compute_cdf_root<double>);
 }
